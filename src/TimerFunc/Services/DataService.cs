b22d3cd0fe41becdad30f1d8f6585717e7845a85
seanr89 <srafferty89@gmail.com>
2024-04-10 20:04:07 +0100

    Update MovieDbService to include authentication logic

--- src/TimerFunc/Services/DataService.cs
+++ src/TimerFunc/Services/DataService.cs
@@ -5,16 +5,21 @@ using Microsoft.Extensions.Logging;
 public class DataService
 {
     private readonly ILogger _logger;
-    public DataService(ILoggerFactory loggerFactory)
+    private readonly MovieDbService _movieDbService;
+    public DataService(ILoggerFactory loggerFactory,
+        MovieDbService movieDbService)
     {
         _logger = loggerFactory.CreateLogger<DataService>();
+        _movieDbService = movieDbService;
     }
 
     public void GetData()
     {
         // Get data from a database or API
-        Thread.Sleep(100);
+        //Thread.Sleep(100);
         _logger.LogInformation($"Data retrieved successfully with env var: " +
             $"{Environment.GetEnvironmentVariable("ENV_VAR", EnvironmentVariableTarget.Process) ?? "Not set"}");
+
+        _movieDbService.Authenticate().Wait();
     }
 }
--- src/TimerFunc/Services/MovieDbService.cs
+++ src/TimerFunc/Services/MovieDbService.cs
@@ -1,24 +1,41 @@
-
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 
 public class MovieDbService
 {
     private readonly HttpClient _httpClient;
+    private readonly ILogger _logger;
     private readonly string _remoteServiceBaseUrl;
 
-    public MovieDbService(HttpClient httpClient)
+    public MovieDbService(HttpClient httpClient, 
+        ILoggerFactory loggerFactory)
     {
         _httpClient = httpClient;
+        _logger = loggerFactory.CreateLogger<MovieDbService>();
+
+        _httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+
+        // using Microsoft.Net.Http.Headers;
+        // The GitHub API requires two headers.
+        _httpClient.DefaultRequestHeaders.Add(
+            HeaderNames.Accept, "application/json");
+
     }
 
-    // public async Task<Catalog> GetCatalogItems(int page, int take,
-    //                                            int? brand, int? type)
-    // {
-    //     var uri = API.Catalog.GetAllCatalogItems(_remoteServiceBaseUrl,
-    //                                              page, take, brand, type);
+    public async Task Authenticate()
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, "authentication");
+        request.Headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzdhZWExMmEzZTJiMTE0NzdlMjcyMjgyNjc4YWIwYSIsInN1YiI6IjY2MTZkOTFjMGYxZTU4MDE3ZDQ2YWYxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K6SGjNgjrbn_PDVV18p5Ay1XRlP73gq2Xzu1WFft80I");
+    
+        var response = await _httpClient.SendAsync(request);
 
-    //     var responseString = await _httpClient.GetStringAsync(uri);
-
-    //     var catalog = JsonConvert.DeserializeObject<Catalog>(responseString);
-    //     return catalog;
-    // }
+        if (response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("Authenticated successfully");
+        }
+        else
+        {
+            Console.WriteLine("Failed to authenticate");
+        }
+    }
 }
